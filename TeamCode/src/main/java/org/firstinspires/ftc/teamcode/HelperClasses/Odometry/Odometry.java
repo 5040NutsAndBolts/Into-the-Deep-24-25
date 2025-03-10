c84a1175ab8c8cf4eaa034268c406e163ba890e1
Sigillian <jdharlow10@gmail.com>
2025-03-10 19:11:32 -0400

    readying roadrunner test branch -> testing soon

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelperClasses/Odometry/Odometry.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.HelperClasses.Odometry;
+public class Odometry {
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelperClasses/Odometry/Roadrunner/util/AxisDirection.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.HelperClasses.Odometry.Roadrunner.util;
+/**
+ * A direction for an axis to be remapped to
+ */
+public enum AxisDirection {
+    POS_X, NEG_X, POS_Y, NEG_Y, POS_Z, NEG_Z
+}
